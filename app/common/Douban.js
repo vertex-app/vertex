1849de2f253477c037dc191953e38c627a7be4a5
栗山未来 <me@mail.lswl.in>
2022-03-14 13:49:48 +0800

    fix: 修复 ttg 搜索种子报错问题

--- app/common/Douban.js
+++ app/common/Douban.js
@@ -170,10 +170,12 @@ class Douban {
           }
           wish.downloaded = await this.selectTorrent(wish);
           this._saveSet();
-          if (!wish.downloaded && !this.selectTorrent[wish.id]) {
+          if (!wish.downloaded) {
             logger.info(this.alias, '未匹配种子', wish.name);
-            this.ntf.selectTorrentError(this.alias, wish);
-            this.selectTorrent[wish.id] = 1;
+            if (!this.selectTorrentToday[wish.id]) {
+              this.ntf.selectTorrentError(this.alias, wish);
+              this.selectTorrentToday[wish.id] = 1;
+            }
           }
         } catch (e) {
           logger.error('豆瓣账户:', this.alias, '选种', wish.name, '失败:\n', e);
