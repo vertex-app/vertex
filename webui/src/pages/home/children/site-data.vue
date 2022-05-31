6ae7898383ee8c0d2b75dd79d12790cfac3a2a3b
栗山未来 <me@mail.lswl.in>
2022-05-31 16:36:30 +0800

    fix: bugfix

--- webui/src/pages/home/children/site-data.vue
+++ webui/src/pages/home/children/site-data.vue
@@ -50,7 +50,7 @@ export default {
           trigger: 'axis',
           formatter: (params) => {
             let str = params[0].axisValue + '</br>';
-            params = params.sort((a, b) => b.value - a.value).slice(10);
+            params = params.sort((a, b) => b.value - a.value).slice(0, 10);
             for (const param of params) {
               const size = this.$formatSize(param.value);
               const increase = this.$formatSize(this.upload.series[param.seriesIndex].data[param.dataIndex] - (this.upload.series[param.seriesIndex].data[param.dataIndex - 1] || 0));
@@ -122,7 +122,7 @@ export default {
           trigger: 'axis',
           formatter: (params) => {
             let str = params[0].axisValue + '</br>';
-            params = params.sort((a, b) => b.value - a.value).slice(10);
+            params = params.sort((a, b) => b.value - a.value).slice(0, 10);
             for (const param of params) {
               const size = this.$formatSize(param.value);
               const increase = this.$formatSize(this.download.series[param.seriesIndex].data[param.dataIndex] - (this.download.series[param.seriesIndex].data[param.dataIndex - 1] || 0));
