cf4b70de77c273bf171b66d9184c27d3b7e35d71
栗山未来 <me@mail.lswl.in>
2023-02-10 19:47:18 +0800

    feat: hhanclub rss 150s 缓存

--- app/libs/rss.js
+++ app/libs/rss.js
@@ -41,7 +41,7 @@ const _getRssContent = async function (rssUrl, suffix = true) {
       body = '<?xml version="1.0" encoding="utf-8"?>\n' + res.body.match(/<rss[\s\S]*<\/rss>/)[0];
     }
     const host = new URL(rssUrl).host;
-    const cacheTime = host.indexOf('lemon') !== -1 ? 150 : 40;
+    const cacheTime = ['lemon', 'hhanclub'].some(item => host.indexOf(item) !== -1) ? 150 : 40;
     await redis.setWithExpire(`vertex:rss:${rssUrl}`, body, isHTML ? 290 : cacheTime);
   }
   return body;
