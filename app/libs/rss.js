e9d29992fe43e9bda83027e351188c66b6a9e325
KyokoMiki <161209740+KyokoMiki@users.noreply.github.com>
2025-05-07 14:38:33 +0800

    feat: anime-no-index.com

--- app/libs/rss.js
+++ app/libs/rss.js
@@ -825,6 +825,7 @@ const _getTorrentsWrapper = {
   'filelist.io': _getTorrentsFileList,
   'blutopia.cc': _getTorrentsUnit3D2,
   'jptv.club': _getTorrentsUnit3D,
+  'anime-no-index.com': _getTorrentsUnit3D2,
   'monikadesign.uk': _getTorrentsUnit3D2,
   'kimoji.club': _getTorrentsKimoji,
   'torrentdb.net': _getTorrentsTorrentDB,
