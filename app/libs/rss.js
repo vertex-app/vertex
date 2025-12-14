7a78fe4bb956535e56dc8d8e12e1f3259181dd40
tong <62124813+voidtao@users.noreply.github.com>
2025-12-14 09:16:12 +0000

    fix: kamept

--- app/libs/rss.js
+++ app/libs/rss.js
@@ -23,15 +23,13 @@ const _getRssContent = async function (rssUrl, suffix = true) {
     let url = rssUrl;
     const isPig = rssUrl.includes('https://piggo.me/');
     const isKamept = rssUrl.includes('https://kamept.com/');
-    let placeholder;
-    if (isKamept) {
-      placeholder = 'placeholder';
-    } else {
-      placeholder = '____';
-    }
-
     if (suffix && !isPig) {
-      url += (rssUrl.indexOf('?') === -1 ? '?' : '&') + `${placeholder}=` + Math.random();
+      if (isKamept) {
+        const randomNum = Math.floor(Math.random() * 100000000);
+        url += '&' + randomNum;
+      } else {
+        url += (rssUrl.indexOf('?') === -1 ? '?' : '&') + '____=' + Math.random();
+      }
     }
     let res;
     if (rssUrl.includes('https://pt.soulvoice.club/') && global.runningSite.SoulVoice) {
